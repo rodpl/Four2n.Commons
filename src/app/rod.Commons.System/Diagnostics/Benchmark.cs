a4d30770a8288961a9e6752e91ef939242cff8f0
Daniel Dabrowski <dabrowski.daniel@gmail.com>
2009-03-16 16:40:26 +0000

    New Benchmark class utility.

---
+++ src/app/rod.Commons.System/Diagnostics/Benchmark.cs
@@ -0,0 +1,102 @@
+﻿// --------------------------------------------------------------------------------------------------------------------- 
+// <copyright file="Benchmark.cs" company="Daniel Dabrowski - rod.blogsome.com">
+//   Copyright (c) Daniel Dabrowski - rod.blogsome.com.  All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the Benchmark type.
+// </summary>
+// ---------------------------------------------------------------------------------------------------------------------
+namespace Rod.Commons.System.Diagnostics
+{
+    using global::System;
+    using global::System.Diagnostics;
+
+    /// <summary>
+    /// Benchmark helper class for measuring performance.
+    /// </summary>
+    public class Benchmark : IDisposable
+    {
+        private readonly MeasureUnitType measureUnitType;
+        private readonly Stopwatch timer = new Stopwatch();
+        private OutputType currentOutputType;
+        private string currentMessageFormat;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Benchmark"/> class.
+        /// </summary>
+        /// <param name="measureUnitType">Type of the measure.</param>
+        private Benchmark(MeasureUnitType measureUnitType)
+        {
+            this.measureUnitType = measureUnitType;
+            this.timer.Start();
+        }
+
+        /// <summary>
+        /// Definition of measure unit types.
+        /// </summary>
+        private enum MeasureUnitType
+        {
+            /// <summary>
+            /// Miliseconds unit.
+            /// </summary>
+            Miliseconds
+        }
+
+        /// <summary>
+        /// Definition of measured output types.
+        /// </summary>
+        private enum OutputType
+        {
+            /// <summary>
+            /// Console output.
+            /// </summary>
+            Console
+        }
+
+        /// <summary>
+        /// Creates <see cref="Benchmark"/> measuring in miliseconds.
+        /// </summary>
+        /// <returns><see cref="Benchmark"/> instance for chaining.</returns>
+        public static Benchmark InMiliseconds()
+        {
+            return new Benchmark(MeasureUnitType.Miliseconds);
+        }
+
+        /// <summary>
+        /// Send benchmark output to the console..
+        /// </summary>
+        /// <param name="messageFormat">The message format.</param>
+        /// <returns><see cref="Benchmark"/> instance for chaining.</returns>
+        public Benchmark ToConsole(string messageFormat)
+        {
+            this.currentMessageFormat = messageFormat;
+            this.currentOutputType = OutputType.Console;
+            this.timer.Reset();
+            this.timer.Start();
+            return this;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.timer.Stop();
+            object duration = null;
+
+            switch (this.measureUnitType)
+            {
+                case MeasureUnitType.Miliseconds:
+                    duration = this.timer.ElapsedMilliseconds;
+                    break;
+            }
+
+            switch (this.currentOutputType)
+            {
+                case OutputType.Console:
+                    Console.Out.WriteLine(this.currentMessageFormat, duration);
+                    break;
+            }
+        }
+    }
+}
