5a6ae79ea1cd9fb2a06e1a39d46df50ba3d5a900
Daniel Dabrowski <dabrowski.daniel@gmail.com>
2010-04-11 22:09:59 +0200

    Added custome dialect for MS SQL 2008 with xml db type.

---
+++ src/app/Rod.Commons.NHibernate/Dialect/MsSql2008Dialect.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MsSql2008Dialect.cs" company="Daniel Dabrowski - rod.blogsome.com">
+//   Copyright (c) Daniel Dabrowski - rod.blogsome.com.  All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the MsSql2008Dialect type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rod.Commons.NHibernate.Dialect
+{
+    using System.Data;
+
+    /// <summary>
+    /// Customized dialect for MS Sql 2008.
+    /// </summary>
+    public class MsSql2008Dialect : global::NHibernate.Dialect.MsSql2008Dialect
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsSql2008Dialect"/> class.
+        /// </summary>
+        public MsSql2008Dialect()
+        {
+            RegisterColumnType(DbType.Xml, "XML");
+        }
+    }
+}
