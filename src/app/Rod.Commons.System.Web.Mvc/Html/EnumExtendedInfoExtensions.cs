785f0b81d2f9dd83fabfcf4256ced86165d5b80c
Daniel Dabrowski <dabrowski.daniel@42n.pl>
2011-06-06 16:15:22 +0200

    Added Html helpers for EnumExtendedInfo rendering.

---
+++ src/app/Rod.Commons.System.Web.Mvc/Html/EnumExtendedInfoExtensions.cs
@@ -0,0 +1,163 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EnumExtendedInfoExtensions.cs" company="Daniel Dabrowski - rod.42n.pl">
+//   Copyright (c) Daniel Dabrowski - rod.42n.pl. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the EnumExtendedInfoExtensions type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rod.Commons.System.Web.Mvc.Html
+{
+    using global::System;
+    using global::System.Collections;
+    using global::System.Collections.Generic;
+    using global::System.Linq.Expressions;
+    using global::System.Text;
+    using global::System.Web.Mvc;
+
+    /// <summary>
+    /// Mvc extensions for <see cref="EnumExtendedInfoAttribute"/>.
+    /// </summary>
+    public static class EnumExtendedInfoExtensions
+    {
+        /// <summary>
+        /// Enums the value info dropdown.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="helper">The helper.</param>
+        /// <param name="name">The name of control.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>Mvc string</returns>
+        public static MvcHtmlString DropdownEnumExtendedInfo<TValue>(this HtmlHelper helper, string name, TValue value)
+                where TValue : struct
+        {
+            return DropdownEnumExtendedInfoInternal(helper, name, value, false, Enum.GetValues(typeof(TValue)));
+        }
+
+        /// <summary>
+        /// Enums the value info dropdown.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="helper">The helper.</param>
+        /// <param name="name">The name of control.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="choices">The choices.</param>
+        /// <returns>Mvc string</returns>
+        public static MvcHtmlString DropdownEnumExtendedInfo<TValue>(this HtmlHelper helper, string name, TValue value, IEnumerable<TValue> choices)
+            where TValue : struct
+        {
+            return DropdownEnumExtendedInfoInternal(helper, name, value, false, choices);
+        }
+
+        /// <summary>
+        /// Enums the value info dropdown.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="helper">The helper.</param>
+        /// <param name="name">The name of control.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>Mvc string</returns>
+        public static MvcHtmlString DropdownEnumExtendedInfo<TValue>(this HtmlHelper helper, string name, TValue? value)
+                where TValue : struct
+        {
+            return DropdownEnumExtendedInfoInternal(helper, name, value, true, Enum.GetValues(typeof(TValue)));
+        }
+
+        /// <summary>
+        /// Enums the value info dropdown.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="helper">The helper.</param>
+        /// <param name="name">The name of control.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="choices">The choices.</param>
+        /// <returns>Mvc string</returns>
+        public static MvcHtmlString DropdownEnumExtendedInfo<TValue>(this HtmlHelper helper, string name, TValue? value, IEnumerable<TValue> choices)
+            where TValue : struct
+        {
+            return DropdownEnumExtendedInfoInternal(helper, name, value, true, choices);
+        }
+
+        /// <summary>
+        /// Enums the value info dropdown for.
+        /// </summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="helper">The helper.</param>
+        /// <param name="expression">The expression.</param>
+        /// <returns> Mvc string </returns>
+        public static MvcHtmlString DropdownEnumExtendedInfoFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
+            where TValue : struct
+        {
+            var data = expression.Compile()(helper.ViewData.Model);
+            var memberExpression = expression.Body as MemberExpression;
+            return DropdownEnumExtendedInfoInternal(helper, memberExpression.Member.Name, data, false, Enum.GetValues(expression.Body.Type));
+        }
+
+        /// <summary>
+        /// Enums the value info dropdown.
+        /// </summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="helper">The helper.</param>
+        /// <param name="expression">Tree expression.</param>
+        /// <returns> Mvc string </returns>
+        public static MvcHtmlString DropdownEnumExtendedInfoFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue?>> expression)
+            where TValue : struct
+        {
+            var data = expression.Compile()(helper.ViewData.Model);
+            var memberExpression = expression.Body as MemberExpression;
+            return DropdownEnumExtendedInfoInternal(helper, memberExpression.Member.Name, data, true, Enum.GetValues(Nullable.GetUnderlyingType(expression.Body.Type)));
+        }
+
+        /// <summary>
+        /// Enums the value info dropdown internal.
+        /// </summary>
+        /// <param name="helper">The helper.</param>
+        /// <param name="name">The name of control.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="isNullable">if set to <c>true</c> [is nullable].</param>
+        /// <param name="choices">The choices.</param>
+        /// <returns>Mvc string</returns>
+        internal static MvcHtmlString DropdownEnumExtendedInfoInternal(HtmlHelper helper, string name, object value, bool isNullable, IEnumerable choices)
+        {
+            StringBuilder build = new StringBuilder();
+            build.AppendFormat("<select name='{0}'>", helper.Encode(name));
+            if (choices != null)
+            {
+                if (isNullable)
+                {
+                    build.AppendOption(string.Empty, string.Empty, value == null);
+                }
+
+                foreach (var choice in choices)
+                {
+                    build.AppendOption(
+                            Convert.ToInt32(choice),
+                            helper.Encode(EnumExtendedInfoAttribute.GetExtendedInfoByEnumValue(choice).Name),
+                            object.Equals(value, choice));
+                }
+            }
+
+            build.Append("</select>");
+            return MvcHtmlString.Create(build.ToString());
+        }
+
+        /// <summary>
+        /// Appends the option.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="text">The text to display.</param>
+        /// <param name="isSelected">if set to <c>true</c> [is selected].</param>
+        private static void AppendOption(this StringBuilder builder, object value, string text, bool isSelected)
+        {
+            builder.AppendFormat(
+                "<option value=\"{0}\"{2}>{1}</option>",
+                value,
+                text,
+                isSelected ? " selected='selected'" : string.Empty);
+        }
+    }
+}
---
+++ src/test/Rod.Commons.System.Web.Mvc.Tests/Html/EnumExtendedInfoExtensionTests.cs
@@ -0,0 +1,126 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EnumExtendedInfoExtensionTests.cs" company="Daniel Dabrowski - rod.42n.pl">
+//   Copyright (c) Daniel Dabrowski - rod.42n.pl. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the EnumExtendedInfoExtensionTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Rod.Commons.System.Web.Mvc.Html
+{
+    using global::System;
+    using global::System.Collections.Generic;
+    using global::System.Linq;
+    using global::System.Text;
+    using global::System.Web.Mvc;
+
+    using NUnit.Framework;
+
+    using UnitTestUtils;
+
+    [TestFixture]
+    public class EnumExtendedInfoExtensionTests
+    {
+        private ViewDataDictionary viewData = new ViewDataDictionary();
+
+        internal enum TestEnum
+        {
+            [EnumExtendedInfo(Name = "Boos", CustomValue = 1)]
+            Boo,
+            [EnumExtendedInfo(CustomValue = "Jeez")]
+            Foo
+        }
+
+        private IEnumerable<TestEnum> GetAll
+        {
+            get
+            {
+                yield return TestEnum.Boo;
+                yield return TestEnum.Foo;
+            }
+        }
+
+        private IEnumerable<TestEnum?> GetAllWithNull
+        {
+            get
+            {
+                yield return null;
+                yield return TestEnum.Boo;
+                yield return TestEnum.Foo;
+            }
+        }
+
+        [Test]
+        public void EnumValueInfoDropdown_WithSampleDataAndPassedPossibleValues()
+        {
+            var helper = MvcHelper.GetHtmlHelper(this.viewData);
+
+            var html = helper.DropdownEnumExtendedInfo("MyName", TestEnum.Boo, new[] { TestEnum.Foo });
+            Assert.AreEqual(@"<select name='MyName'><option value=""1"">Foo</option></select>", html.ToHtmlString());
+        }
+
+        [Test]
+        public void EnumValueInfoDropdown_WithSampleSelectedDataAndPassedPossibleValues()
+        {
+            var helper = MvcHelper.GetHtmlHelper(this.viewData);
+
+            var html = helper.DropdownEnumExtendedInfo("MyName", TestEnum.Foo, new[] { TestEnum.Foo });
+            Assert.AreEqual(@"<select name='MyName'><option value=""1"" selected='selected'>Foo</option></select>", html.ToHtmlString());
+        }
+
+        [Test]
+        public void EnumValueInfoDropdown_WithSampleDataWithoutPassedPossibleValues()
+        {
+            var helper = MvcHelper.GetHtmlHelper(this.viewData);
+
+            var html = helper.DropdownEnumExtendedInfo("MyName", TestEnum.Boo);
+            Assert.AreEqual(@"<select name='MyName'><option value=""0"" selected='selected'>Boos</option><option value=""1"">Foo</option></select>", html.ToHtmlString());
+        }
+
+        [Test]
+        public void EnumValueInfoDropdown_WithSampleNullableData()
+        {
+            var helper = MvcHelper.GetHtmlHelper(this.viewData);
+
+            var html = helper.DropdownEnumExtendedInfo("MyName", (TestEnum?)null, this.GetAll);
+            Assert.AreEqual(@"<select name='MyName'><option value="""" selected='selected'></option><option value=""0"">Boos</option><option value=""1"">Foo</option></select>", html.ToHtmlString());
+            Console.Out.WriteLine(html.ToHtmlString());
+        }
+
+        [Test]
+        public void EnumValueInfoDropdownFor_NonNullable_WithSampleValue()
+        {
+            var model = new SampleModel();
+            model.SomeOption = TestEnum.Foo;
+
+            var helper = MvcHelper.GetHtmlHelper(new ViewDataDictionary<SampleModel>(model));
+
+            var html = helper.DropdownEnumExtendedInfoFor(x => x.SomeOption);
+
+            Assert.AreEqual(@"<select name='SomeOption'><option value=""0"">Boos</option><option value=""1"" selected='selected'>Foo</option></select>", html.ToHtmlString());
+            Console.Out.WriteLine(html.ToHtmlString());
+        }
+
+        [Test]
+        public void EnumValueInfoDropdownFor_Nullable_WithSampleValue()
+        {
+            var model = new SampleModel();
+            model.SomeNullableOption = TestEnum.Foo;
+
+            var helper = MvcHelper.GetHtmlHelper(new ViewDataDictionary<SampleModel>(model));
+
+            var html = helper.DropdownEnumExtendedInfoFor(x => x.SomeNullableOption);
+
+            Assert.AreEqual(@"<select name='SomeNullableOption'><option value=""""></option><option value=""0"">Boos</option><option value=""1"" selected='selected'>Foo</option></select>", html.ToHtmlString());
+            Console.Out.WriteLine(html.ToHtmlString());
+        }
+
+        internal class SampleModel
+        {
+            internal TestEnum SomeOption { get; set; }
+
+            internal TestEnum? SomeNullableOption { get; set; }
+        }
+    }
+}
