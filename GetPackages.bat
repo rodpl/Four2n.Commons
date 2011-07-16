3355494fb32abd08ea02d1e02feb6174cf699f67
Daniel Dabrowski <dabrowski.daniel@42n.pl>
2011-07-16 02:11:02 +0200

    Added missing Moq package

--- GetPackages.bat
+++ GetPackages.bat
@@ -3,3 +3,4 @@ SET NUGET=%~dp0\tools\nuget\NuGet.exe
 %NUGET% i NHibernate -v 2.1.2.4000 -o packages
 %NUGET% i SqlServerCompact -v 4.0.8482.1 -o packages
 %NUGET% i nunit -v 2.5.10.11092 -o packages
+%NUGET% i Moq -v 4.0.8435.1 -o packages
